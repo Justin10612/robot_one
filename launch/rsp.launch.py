ed9009b9b78a532626e52a2710a05f91bb5843ee
Justin10612 <atom.9031@gmail.com>
2023-04-27 14:38:34 +0800

    start ros2_control

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -14,13 +14,13 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
-    # use_ros2_control = LaunchConfiguration('use_ros2_control')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('robot_one'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file).toxml()
-    #robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
+    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
@@ -49,10 +49,10 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-        # DeclareLaunchArgument(
-        #     'use_ros2_control',
-        #     default_value='true',
-        #     description='Use ros2_control if true'),
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='Use ros2_control if true'),
 
         node_robot_state_publisher,
         #node_joint_state_publisher_gui
