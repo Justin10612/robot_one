3599fa58e68606eedc8cec4364b58f1cfa27c0ca
Justin10612 <atom.9031@gmail.com>
2023-04-27 18:00:23 +0800

    Another Giant leap, ros2_con sucess

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,8 +1,8 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
-
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -44,8 +44,9 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')])
+                    # ,
+                    # launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -59,6 +60,13 @@ def generate_launch_description():
         package="controller_manager",
         executable="spawner.py",
         arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[diff_drive_spawner],
+        )
     )
 
     joint_broad_spawner = Node(
@@ -89,10 +97,10 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        # joystick,
-        # twist_mux,
+        #joystick,
+        #twist_mux,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        joint_broad_spawner,
+        diff_drive_spawner
     ])
--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('robot_one'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    #robot_description_config = xacro.process_file(xacro_file).toxml()
     robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
@@ -41,7 +41,6 @@ def generate_launch_description():
             executable='rviz2',
             name='rviz2',
     )
-    #test
 
     # Launch!
     return LaunchDescription([
