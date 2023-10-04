9c38b2b0569bbe0ae35950d5c2ec66afbe7db12f
Justin10612 <atom.9031@gmail.com>
2023-10-05 01:54:05 +0800

    None

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -26,11 +26,11 @@ def generate_launch_description():
         ), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    ros2_agent = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [os.path.join(get_package_share_directory(package_name),'launch','launch_stm32.launch.py')]
-        )
-    )
+    # ros2_agent = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         [os.path.join(get_package_share_directory(package_name),'launch','launch_stm32.launch.py')]
+    #     )
+    # )
 
     joystick = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
@@ -38,13 +38,13 @@ def generate_launch_description():
         )]), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params, {'use_sim_time': True}],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
+    # twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    # twist_mux = Node(
+    #         package="twist_mux",
+    #         executable="twist_mux",
+    #         parameters=[twist_mux_params, {'use_sim_time': True}],
+    #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    #     )
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','robot_control.yaml')
@@ -86,7 +86,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        joystick,
+        # joystick,
         # twist_mux,
         delayed_controller_manager,
         delayed_joint_broad_spawner,
